c71ab844fb2fec7b38b0f8a18ecdbf9d2c209a5f
DEEPTI P RANJOLKAR <deeptiranjolkar123@gmail.com>
2023-10-13 13:11:46 +0530

    setting up the server

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+
+const app = express();
+
