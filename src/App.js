b83ec8da53754651a23d9c3912d9dc09bb6bdfb7
msaichaitra <msaichaitra@gmail.com>
2023-10-13 11:17:14 +0530

    added by chaitra

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 style={{textAlign:"center"}}>Hotel Inventory Management System</h1>
+    <h1> Hi - By Chaitra </h1>
+    </>
+  );
+}
+
+export default App;
