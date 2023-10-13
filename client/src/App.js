8b366d1f8304855d37302b57dc5a7e6a202c509c
DEEPTI P RANJOLKAR <deeptiranjolkar123@gmail.com>
2023-10-13 15:07:31 +0530

    "server is set up"

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 
+
 function App() {
   return (
     <>
     <h1 style={{textAlign:"center"}}>Hotel Inventory Management System</h1>
+    <h1>hio there</h1>
     </>
   );
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,28 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
+const cors = require('cors');
+dotenv.config()
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.json({
+        status:'success',
+        message:"all good"
+    })
+})
+
+app.listen(process.env.PORT,()=>{
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>{
+        console.log(`server runnning on http://localhost:${process.env.PORT}`)
+    }).catch(error =>console.log(error))
+  
+})
 
