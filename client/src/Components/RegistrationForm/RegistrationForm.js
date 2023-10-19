6ee3161c222a67439c0049776f475493fe2e37d8
DEEPTI P RANJOLKAR <deeptiranjolkar123@gmail.com>
2023-10-19 17:46:17 +0530

    registration form created successfully

--- client/src/Components/RegistrationForm/RegistrationForm.js
+++ client/src/Components/RegistrationForm/RegistrationForm.js
@@ -1,37 +1,137 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-// CSS styles for the component
 import './RegistrationForm.css';
 
 function RegistrationForm() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    contactNumber: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [error, setError] = useState('');
+
+  const handleFormSubmit = async () => {
+    if (validateForm()) {
+      try {
+        const response = await axios.post('http://localhost:9000/api/register', formData);
+        console.log('Registration successful:', response.data);
+      } catch (error) {
+        console.error('Registration failed:', error);
+      }
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const validateForm = () => {
+    if (
+      formData.firstName === '' ||
+      formData.lastName === '' ||
+      formData.contactNumber === '' ||
+      formData.email === '' ||
+      formData.password === '' ||
+      formData.confirmPassword === ''
+    ) {
+      setError('Please fill in all required fields.');
+      return false;
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(formData.email)) {
+      setError('Please enter a valid email address.');
+      return false;
+    } else if (formData.password !== formData.confirmPassword) {
+      setError('Password and confirm password do not match.');
+      return false;
+    }
+
+    setError(''); // Clear any previous error messages
+    return true;
+  };
+
   return (
     <div className="container">
-        <br></br>
+      <br></br>
       <h3>Registration Form</h3>
       <br />
       <div className="input-container">
         <div className="input-row">
-          Enter your First Name: <input placeholder='ex: john' />
+          Enter your First Name:
+          <input
+            name="firstName"
+            placeholder="ex: john"
+            value={formData.firstName}
+            onChange={handleInputChange}
+            required
+          />
         </div>
         <br></br>
         <div className="input-row">
-          Enter your Last Name: <input placeholder='ex: cena' />
+          Enter your Last Name:
+          <input
+            name="lastName"
+            placeholder="ex: cena"
+            value={formData.lastName}
+            onChange={handleInputChange}
+            required
+          />
         </div>
         <br></br>
         <div className="input-row">
-          Enter your Contact number: <input placeholder='ex. 9090909090' />
+          Enter your Contact number:
+          <input
+            name="contactNumber"
+            placeholder="ex. 9090909090"
+            value={formData.contactNumber}
+            onChange={handleInputChange}
+            required
+          />
         </div>
         <br></br>
         <div className="input-row">
-          Enter Password: <input placeholder='anything@#123' />
+          Enter your Email:
+          <input
+            name="email"
+            type="email"
+            placeholder="ex. xxx@gmail.com"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
         </div>
         <br></br>
         <div className="input-row">
-          Enter the Password again: <input placeholder='anything@#123' />
+          Enter Password:
+          <input
+            name="password"
+            placeholder="anything@#123"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <br></br>
+        <div className="input-row">
+          Enter the Password again:
+          <input
+            name="confirmPassword"
+            placeholder="anything@#123"
+            value={formData.confirmPassword}
+            onChange={handleInputChange}
+            required
+          />
         </div>
       </div>
       <br></br>
-      <button className="btn">Enter</button>
+      <div className="error-message" style={{ color: 'red' }}>{error}</div>
+      <button className="btn" onClick={handleFormSubmit}>
+        Enter
+      </button>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -12,6 +12,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors())
+//creating a schema for users
+const User = mongoose.model('User',{
+    firstName:String,
+    lastName:String,
+    contactNumber:String,
+    Email:{ type: String, unique: true },
+    password:String,
+    passwordAgain:String,
+})
+
+
+
 
 app.get('/',(req,res)=>{
     res.json({
@@ -19,6 +31,32 @@ app.get('/',(req,res)=>{
         message:"all good"
     })
 })
+
+
+
+app.post('/api/register', async(req, res) => {
+    try{
+    const { firstName, lastName, contactNumber, email, password ,confirmPassword} = req.body;
+    if (password !== confirmPassword) {
+        return res.status(400).json({ error: 'Password and confirm password do not match' });
+      }
+    const user = new User({
+        firstName,
+        lastName,
+        contactNumber,
+        Email: email, 
+        password,
+        passwordAgain:confirmPassword, 
+      });
+      
+    await user.save();
+
+    res.json({ message: 'Registration successful' });
+  } catch (error) {
+    console.error('Registration failed:', error);
+    res.status(500).json({ error: 'Registration failed' });
+  }
+  });
 
 app.listen(process.env.PORT,()=>{
     mongoose.connect(process.env.MONGODB_URL)
